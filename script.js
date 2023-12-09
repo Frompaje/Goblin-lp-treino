46eeeb22843cdecdd10712a7dc5dc0eef6879150
Edwards Yan <117830579+Frompaje@users.noreply.github.com>
2023-12-09 16:56:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector("#menu-icon")
+let navBar = document.querySelector(".navbar ul")
+
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x")
+    navBar.classList.toggle("open")
+}
+
+const sr = ScrollReveal({
+    distance: "65px",
+    duration: 2600,
+    delay: 450,
+    reset: true
+})
+
+
+
+
+
+
+sr.reveal(".hero-text", { delay: 200, origin: "top" })
+sr.reveal(".hero-img", { delay: 200, origin: "top" })
+sr.reveal(".icons a ", { delay: 500, origin: "left" })
+sr.reveal(".scroll-down a", { delay: 500, origin: "right" })
